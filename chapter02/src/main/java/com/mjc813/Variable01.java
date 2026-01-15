088571f4a5c47b214419784c2145da185af706ae
saehan03 <saehan0512@gmail.com>
2026-01-15 13:00:25 +0900

    chapter02

---
+++ chapter02/src/main/java/com/mjc813/Variable01.java
@@ -0,0 +1,27 @@
+package com.mjc813;
+
+public class Variable01 {
+    public void howToUseVariable() {
+        int value = 0;
+
+        int result = value + 10;
+
+        System.out.println(result);
+
+        int hour = 3;
+        int minute = 5;
+        System.out.println(hour + "시간" + minute + "분");
+
+        int totalMinute = (hour*60) + minute;
+        System.out.println("총 " + totalMinute + "분");
+
+        int x = 3;
+        int y = 5;
+        System.out.println("x:" + x + ", y:" + y);
+
+        int temp = x;
+        x = y;
+        y = temp;
+        System.out.println("x:" + x + ", y:" + y);
+    }
+}
