5b23d64e9fa79421aec83cb28194be725cd2a778
saehan03 <saehan0512@gmail.com>
2026-01-27 12:18:27 +0900

    Ch07 p.294 예제

--- Chapter07/src/main/java/com/mjc813/Phone.java
+++ Chapter07/src/main/java/com/mjc813/Phone.java
@@ -11,7 +11,9 @@ public class Phone {
     public String model;
     public String color;
 
-    public Phone() {
-        System.out.println("Phone() 생성자 실행");
+    public Phone(String model, String color) {
+        this.setModel(model);
+        this.setColor(color);
+        System.out.println("Phone(String model, String color) 생성자 실행");
     }
 }
--- Chapter07/src/main/java/com/mjc813/SmartPhone.java
+++ Chapter07/src/main/java/com/mjc813/SmartPhone.java
@@ -9,9 +9,9 @@ import lombok.Setter;
 public class SmartPhone extends Phone {
 
     public SmartPhone(String model, String color) {
-        super();
-        this.setModel(model);
-        this.setColor(color);
+        super(model, color);
+//        this.setModel(model);
+//        this.setColor(color);
         System.out.println("SmartPhone(String model, String color) 생성자 실행됨");
     }
 }
