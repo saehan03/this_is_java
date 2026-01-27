bf1bfa98d83c12cf000baf6eea7b55b16f2e84ed
saehan03 <saehan0512@gmail.com>
2026-01-27 12:12:32 +0900

    Revert "Ch07 예제 p.293"
    This reverts commit 818480f147e28c250dacf8a586064a60dc98d521.

--- Chapter07/src/main/java/com/mjc813/SmartPhone.java
+++ Chapter07/src/main/java/com/mjc813/SmartPhone.java
@@ -7,11 +7,19 @@ import lombok.Setter;
 @Setter
 
 public class SmartPhone extends Phone {
+    public boolean wifi;
 
     public SmartPhone(String model, String color) {
-        super();
-        this.setModel(model);
-        this.setColor(color);
-        System.out.println("SmartPhone(String model, String color) 생성자 실행됨");
+        this.model = model;
+        this.color = color;
+    }
+
+    public void setWifi(boolean wifi) {
+        this.wifi = wifi;
+        System.out.println("와이파이 상태를 변경했습니다.");
+    }
+
+    public void internet() {
+        System.out.println("인터넷에 연결합니다.");
     }
 }
