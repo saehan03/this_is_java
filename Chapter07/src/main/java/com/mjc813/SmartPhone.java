2a04a9c03f15816cab4d1db208ca77a93835cb98
saehan03 <saehan0512@gmail.com>
2026-01-27 12:13:30 +0900

    Reapply "Ch07 예제 p.293"
    This reverts commit bf1bfa98d83c12cf000baf6eea7b55b16f2e84ed.

--- Chapter07/src/main/java/com/mjc813/SmartPhone.java
+++ Chapter07/src/main/java/com/mjc813/SmartPhone.java
@@ -7,19 +7,11 @@ import lombok.Setter;
 @Setter
 
 public class SmartPhone extends Phone {
-    public boolean wifi;
 
     public SmartPhone(String model, String color) {
-        this.model = model;
-        this.color = color;
-    }
-
-    public void setWifi(boolean wifi) {
-        this.wifi = wifi;
-        System.out.println("와이파이 상태를 변경했습니다.");
-    }
-
-    public void internet() {
-        System.out.println("인터넷에 연결합니다.");
+        super();
+        this.setModel(model);
+        this.setColor(color);
+        System.out.println("SmartPhone(String model, String color) 생성자 실행됨");
     }
 }
