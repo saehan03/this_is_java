f69a148dec05913f01be0996cdf00e946219c5f3
saehan03 <saehan0512@gmail.com>
2026-01-27 11:40:38 +0900

    Chapter 07 시작

---
+++ Chapter07/src/main/java/com/mjc813/Main.java
@@ -0,0 +1,11 @@
+package com.mjc813;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Chapter07 sp = new Chapter07();
+        sp.SmartPhoneEx();
+    }
+}
---
+++ Chapter07/src/main/java/com/mjc813/Phone.java
@@ -0,0 +1,29 @@
+package com.mjc813;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Phone {
+
+    public String model;
+    public String color;
+
+    public void bell() {
+        System.out.println("벨이 울립니다.");
+    }
+
+    public void sendVoice(String message) {
+        System.out.println("자기: " + message);
+    }
+
+    public void receiveVoice(String message) {
+        System.out.println("상대방: " + message);
+    }
+
+    public void hangUp() {
+        System.out.println("전화를 끊습니다.");
+    }
+}
