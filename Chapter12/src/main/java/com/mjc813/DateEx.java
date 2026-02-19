2485f1700cc7bbd0c3ecb5e1d8c2d4ef3fe88abd
saehan03 <saehan0512@gmail.com>
2026-02-19 12:37:11 +0900

    12장 예제 p538까지

---
+++ Chapter12/src/main/java/com/mjc813/DateEx.java
@@ -0,0 +1,16 @@
+package com.mjc813;
+
+import java.text.*;
+import java.util.*;
+
+public class DateEx {
+    public static void main(String[] args) {
+        Date now = new Date();
+        String strNow1 = now.toString();
+        System.out.println(strNow1);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        String strNow2 = sdf.format(now);
+        System.out.println(strNow2);
+    }
+}
