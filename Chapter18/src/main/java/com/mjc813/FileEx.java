9ca770c2096099a3aed23b63eea9201e13c50822
saehan03 <saehan0512@gmail.com>
2026-03-10 17:02:12 +0900

    Ch18 예제 마무리

--- Chapter18/src/main/java/com/mjc813/FileEx.java
+++ Chapter18/src/main/java/com/mjc813/FileEx.java
@@ -1,11 +1,31 @@
-package com.mjc813;
-
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-public class FileEx {
-    public static void main(String[] args) throws Exception {
-        File dir = new File("C:/Temp")
-    }
-}
+//package com.mjc813;
+//
+//import java.io.File;
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//
+//public class FileEx {
+//    public static void main(String[] args) throws Exception {
+//        File dir = new File("C:/Temp/images");
+//        File file1 = new File("C:/Temp/file1.txt");
+//        File file2 = new File("C:/Temp/file2.txt");
+//        File file3 = new File("C:/Temp/file3.txt");
+//
+//        if(dir.exists() == false) { dir.mkdir(); }
+//        if(file1.exists() == false) { file1.createNewFile(); }
+//        if(file2.exists() == false) { file2.createNewFile(); }
+//        if(file3.exists() == false) { file3.createNewFile(); }
+//
+//        File temp = new File("C:/Temp");
+//        File[] contents = temp.listFile();
+//
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd a HH:mm");
+//        for(File file : contents) {
+//            System.out.printf("%-25s", sdf.format(new Date(file.lastModified())));
+//            if(file.isDirectory()) {
+//                System.out.printf("%-10s%s20s", file.length(), file.getName());
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
