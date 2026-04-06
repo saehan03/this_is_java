7e362b583a98f1446555896c35693d0fdbc68ee1
saehan03 <saehan0512@gmail.com>
2026-04-06 21:32:24 +0900

    Exceptionhandler json 처리

--- PetApp/src/main/java/com/mjc813/petapp/pet/cntr/PetRestController.java
+++ PetApp/src/main/java/com/mjc813/petapp/pet/cntr/PetRestController.java
@@ -1,5 +1,6 @@
 package com.mjc813.petapp.pet.cntr;
 
+import com.mjc813.petapp.common.MyException;
 import com.mjc813.petapp.pet.PetRequestDto;
 import com.mjc813.petapp.pet.PetResponseDto;
 import com.mjc813.petapp.pet.dto.PetDto;
@@ -57,66 +58,66 @@ public class PetRestController {
      */
     @PostMapping
     public ResponseEntity<PetResponseDto> insertPet(@RequestBody PetDto petDto) {
-        try {
+//        try {
             PetDto result = this.petService.insert(petDto);
             return ResponseEntity.ok().body(new PetResponseDto(0, "SUCCESS", result));
-        } catch (NoSuchElementException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new PetResponseDto(-997, "Not found error", null));
-        } catch (Exception ex) {
-            return ResponseEntity.status(500).body(new PetResponseDto(-999, "ERROR", null));
-        }
+//        } catch (NoSuchElementException e) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new PetResponseDto(-997, "Not found error", null));
+//        } catch (Exception ex) {
+//            return ResponseEntity.status(500).body(new PetResponseDto(-999, "ERROR", null));
+//        }
     }
 
     @PatchMapping
-    public ResponseEntity<PetResponseDto> updatePet(@RequestBody PetDto petDto) {
+    public ResponseEntity<PetResponseDto> updatePet(@RequestBody PetDto petDto) throws MyException {
         try {
-            PetDto result = this.petService.update(petDto);
-            return  ResponseEntity.ok().body(new PetResponseDto(0, "SUCCESS", result));
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body(new PetResponseDto(-999, "ERROR", null));
+        PetDto result = this.petService.update(petDto);
+        return ResponseEntity.ok().body(new PetResponseDto(0, "SUCCESS", result));
+    } catch (NoSuchElementException e) {
+            throw new MyException(petDto);
         }
     }
 
     @DeleteMapping
     public ResponseEntity<PetResponseDto> deletePet(@RequestParam Integer id) {
-        try {
+//        try {
             PetDto result = this.petService.deleteById(id);
             return ResponseEntity.ok().body(new PetResponseDto(0, "SUCCESS", result));
-        } catch (NoSuchElementException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new PetResponseDto(-997, "Not found error", null));
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body(new PetResponseDto(-999, "ERROR", null));
-        }
+//        } catch (NoSuchElementException e) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new PetResponseDto(-997, "Not found error", null));
+//        } catch (Exception e) {
+//            return ResponseEntity.status(500).body(new PetResponseDto(-999, "ERROR", null));
+//        }
     }
 
     @GetMapping("/{id}")    // => /api/pet?id=번호  => /api/pet/번호
     public ResponseEntity<PetResponseDto> findById(@PathVariable Object id) {
-        try {
+//        try {
             Integer nId = Integer.parseInt(id.toString());
             PetDto result = this.petService.findById(nId);
             return ResponseEntity.ok().body(new PetResponseDto(0, "SUCCESS", result));
-        } catch (NoSuchElementException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new PetResponseDto(-997, "Not found error", null));
-        } catch (NumberFormatException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new PetResponseDto(-994, "Number error", null));
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body(new PetResponseDto(-999, "ERROR", null));
-        }
+//        } catch (NoSuchElementException e) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new PetResponseDto(-997, "Not found error", null));
+//        } catch (NumberFormatException e) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new PetResponseDto(-994, "Number error", null));
+//        } catch (Exception e) {
+//            return ResponseEntity.status(500).body(new PetResponseDto(-999, "ERROR", null));
+//        }
     }
 
     @GetMapping("/search")
     public ResponseEntity<PetResponseDto> findByNameContains(
-            @RequestParam(name="searchName", defaultValue = "") String searchName
+            @RequestParam(name="sn", defaultValue = "") String searchName
             , @PageableDefault(size=5, sort="id", direction = Sort.Direction.DESC) Pageable pageable)
     // 웹클라이언트에서 GET주소 요청시에 Pageable 정보를 이런식으로 전달할 수 있다. &sort=id,desc&size=4&page=0
     {
-        try {
+//        try {
             PetRequestDto prd = new PetRequestDto();
             prd.setSearchName(searchName);
             Slice<PetEntity> result = this.petService.findByNameContains(prd, pageable);
             return ResponseEntity.ok().body(new PetResponseDto(0, "SUCCESS", result));
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body(new PetResponseDto(-999, "ERROR", null));
-        }
+//        } catch (Exception e) {
+//            return ResponseEntity.status(500).body(new PetResponseDto(-999, "ERROR", null));
+//        }
     }
 }
