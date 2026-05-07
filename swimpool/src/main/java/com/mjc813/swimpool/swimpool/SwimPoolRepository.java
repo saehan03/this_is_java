58e2b8de224d17976699c794669045f5f9d2cbe2
saehan03 <saehan0512@gmail.com>
2026-05-07 20:08:29 +0900

    swim 수정 2

--- swimpool/src/main/java/com/mjc813/swimpool/swimpool/SwimPoolRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mjc813.swimpool.swimpool;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SwimPoolRepository extends JpaRepository<SwimPoolEntity, Long> {
-}
